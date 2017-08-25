c3a107a6ea6fd7a5aa701ad2ef228b459e8ca5bb
aluno <aluno@LAB104-06>
2017-08-24 21:23:45 -0300

    Começando o primeiro app, inicio de código.

---
+++ App1/App1/MinhaPagina.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public class MinhaPagina: ContentPage 
+    {
+        public MinhaPagina()
+        {
+            Content: new StackLayout
+        }
+
+    }
+}
